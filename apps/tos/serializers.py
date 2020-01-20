c7f26090f20ae9efaf781e026e5930e6f4ff94aa
goutom <goutom.sust.cse@gmail.com>
2020-01-20 22:28:32 +0600

    Modified README.md

--- apps/tos/serializers.py
+++ apps/tos/serializers.py
@@ -1,6 +1,4 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from apps.main.models import UserProfile
 from apps.tos.models import TermsOfService, UserTermsOfService
 
 
